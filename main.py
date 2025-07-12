df72bef728fbbf39af4a921a6d102ee6c2a1a9f2
PeterMagdik <peter.magdik@student.ssnd.sk>
2025-07-12 16:08:02 +0200

    feat: add manual input

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-note_string = "B B ^C#-^D-B ^D ^E ^C#-B ^C#-B-A"
-
-TONE_FREQUENCIES = {
-    "A": 440,
-    "B": 493,
-    "C": 523,
-    "D": 587,
-    "E": 659,
-    "F": 698,
-    "G": 784,
-}
-
 def count_spaces_between_letters(note_string: str, start_index: int) -> int:
     """
     Count spaces between the current note and the next one in the string.
     Used to determine pause duration between notes.
     """
+
     for next_index in range(start_index + 1, len(note_string)):
         if note_string[next_index].isalpha():
             return note_string[start_index + 1:next_index].count(" ")
@@ -24,6 +13,7 @@ def is_sharp_note(notes: str, index: int) -> bool:
     """
     Checks if the note at a given index is sharp.
     """
+    
     return (index > 0 and notes[index - 1] == "^") or (index + 1 < len(notes) and notes[index + 1] == "#")
 
 def get_frequency_pause(notes: str) -> tuple[list[int], list[float]]:
@@ -31,6 +21,17 @@ def get_frequency_pause(notes: str) -> tuple[list[int], list[float]]:
     Converts a string of musical notes into a sequence of [frequency, pause_duration] pairs.
     Supports sharps (^A or A#), and pauses indicated by '-' after a note.
     """
+    
+    TONE_FREQUENCIES = {
+        "A": 440,
+        "B": 493,
+        "C": 523,
+        "D": 587,
+        "E": 659,
+        "F": 698,
+        "G": 784,
+    }
+
     result = []
 
     for i, char in enumerate(notes):
@@ -53,6 +54,7 @@ def get_frequency_pause(notes: str) -> tuple[list[int], list[float]]:
     return tuple(result)
 
 def main():
+    note_string = input("Insert notes: ")
     sequence = get_frequency_pause(note_string)
     print(sequence)
 
